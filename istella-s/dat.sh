f88dfb4f7fe6a13b47eda6ee2004603d48b3e8bf
Luke Gallagher <luke@hypergeometric.net>
2018-03-06 21:39:49 +1100

    Istella-S: LambdaRank (LightGBM)

---
+++ istella-s/dat.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+SPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE=$SPATH/..
+DATDIR=$SPATH/dat
+
+cd $DATDIR
+T=test.txt
+V=vali.txt
+TR=train.txt
+
+(test ! -f $T \
+    || test ! -f $V \
+    || test ! -f $TR) \
+    && echo "Data files required in $DATDIR" 1>&2 \
+    && exit 1
+
+# xgboost, lgbm
+cut -d' ' -f1,3- $T > $T.xgb
+awk '{sub(/qid:/, "", $2); print $2;}' $T | uniq -c | awk '{print $1}' > $T.xgb.group
+cut -d' ' -f1,3- $V > $V.xgb
+awk '{sub(/qid:/, "", $2); print $2;}' $V | uniq -c | awk '{print $1}' > $V.xgb.group
+cut -d' ' -f1,3- $TR > $TR.xgb
+awk '{sub(/qid:/, "", $2); print $2;}' $TR | uniq -c | awk '{print $1}' > $TR.xgb.group
+cp $T.xgb.group $T.xgb.query
+cp $V.xgb.group $V.xgb.query
+cp $TR.xgb.group $TR.xgb.query
+
+# jforest
+$BASE/script/jfmkbin.sh $DATDIR $T $V $TR
+
+$BASE/script/svm2qrel.sh $T > test.qrels
